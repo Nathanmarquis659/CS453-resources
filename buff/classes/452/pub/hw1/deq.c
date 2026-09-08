4e27ded6cc92847be1e825ea90e37737510cdb5c
nathanmarquis659 <nathanmarquis659@u.boisestate.edu>
2026-09-07 19:27:10 -0600

    proper error messages

--- buff/classes/452/pub/hw1/deq.c
+++ buff/classes/452/pub/hw1/deq.c
@@ -66,7 +66,7 @@ static Data ith(Rep r, End e, int i)
   // Check if index > length-1 (out of bounds)
   if (i > r->len - 1 || i < 0)
   {
-    ERROR("Index out of bounds");
+    ERROR("index out of range");
   }
 
   Node n = r->ht[e];
@@ -82,7 +82,7 @@ static Data ith(Rep r, End e, int i)
 static Data get(Rep r, End e)
 {
   // Check if length is 0
-  if (r->len == 0) ERROR("Empty queue");
+  if (r->len == 0) ERROR("zero pointer");
   // lastNode = pointer to Rep->ht[End]
   Node lastNode = r->ht[e];
   Node prevNode = r->ht[e]->np[inverseEnd(e)];
@@ -114,7 +114,7 @@ static Data get(Rep r, End e)
 static Data rem(Rep r, End e, Data d)
 {
   // if length = 0 return 0
-  if (r->len == 0) return 0;
+  if (r->len == 0) ERROR("zero pointer");
 
   Node n = r->ht[e];
   // From end, move inward for index steps
